db261be68ee8ed2c048188f626af10bf02446d3b
Kavya-Sree-Gundu <kavyasreegundu0gmail.com>
2026-09-05 16:33:29 +0530

    Functions

---
+++ day-17/function.py
@@ -0,0 +1,9 @@
+def display(name,email,password):
+    print(f'Hello {name}')
+    print(f'Your email: {email}')
+    print(f'Your password: {password}')
+
+
+display('Kavya','Kavya@email,com','Kavya@123')
+display('ankitha','ankitha@gmail.com','ankitha@123')
+display('shivani','shivani@gmail.com','shivani@123')
