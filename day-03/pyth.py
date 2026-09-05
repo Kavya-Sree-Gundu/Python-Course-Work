8bec86e7454be6e97ce6f70ced4687edcc66ba72
Kavya-Sree-Gundu <kavyasreegundu0gmail.com>
2026-09-05 15:31:08 +0530

    Data Types

---
+++ day-03/pyth.py
@@ -0,0 +1,58 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+count = 10
+count =7
+count
+7
+type(count)
+<class 'int'>
+<class 'int'
+SyntaxError: invalid syntax
+price =99.99
+price
+99.99
+type(price)
+<class 'float'>
+c + 3+8j
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    c + 3+8j
+NameError: name 'c' is not defined
+c = 3+8j
+c
+(3+8j)
+c = 4+9j
+c
+(4+9j)
+type(c)
+<class 'complex'>
+s ='codegnan'
+type(s)
+<class 'str'>
+s="Code"
+type(s)
+<class 'str'>
+1 = []
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+l = []
+1 =list()
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+1 =List()
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+l =List
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    l =List
+NameError: name 'List' is not defined. Did you mean: 'list'?
+l =list()
+type(1)
+<class 'int'>
+1 =[1,2,3,4,5,"dfghjk",78,678,[1,2,4],(1,2)]
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+l =[1,2,2,4,4,5,"dfghtj",78,678,[1,2,4],(1,2,1)]
+type(1)
+<class 'int'>
+t=(1)
+t=tuple()
+type=(t)
+t=[1,2,3,4,5,"qwert",77,20,(1,2)]
