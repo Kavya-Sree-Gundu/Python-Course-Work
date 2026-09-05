8ab642ed5f9b26c0eb6f47ad48dad47b7c5b3c89
Kavya-Sree-Gundu <kavyasreegundu0gmail.com>
2026-09-05 16:36:57 +0530

    Types of arg

---
+++ day-19/typesofarg.py
@@ -0,0 +1,75 @@
+'''def display(name,email,password):
+    print(f'name: {name}')
+    print(f'email: {email}')
+    print(f'password: {password}')
+
+display('kavya','kavya@gmail','kavya@123')
+display('pavani','pavani@gmail','pavani@123')
+display('ankitha','ankitha@gmail','ankitha@123')
+'''
+'''def display(name,email,password):
+    print(f'name: {name}')
+    print(f'email: {email}')
+    print(f'password: {password}')
+
+display(name='kavya',email='kavya@gmail',password='kavya@123')
+display(name='pavani',email='pavani@gmail',password='pavani@123')
+display(name='ankitha',email='ankitha@gmail',password='anitha@123')
+'''
+
+'''def display(name,email='gmail.com',password=''):
+    print(f'name: {name}')
+    print(f'email: {email}')
+    print(f'password: {password}')
+
+display('kavya','kavya@gmail.com','kavya@123')
+display('kavya','kavya@gmail.com')
+display('kavya')
+'''
+
+'''def display(*names):
+    print(names)
+
+
+display('kavya')
+display('kavya','pavani')
+display('kavya','pavani','ankitha')
+display('kavya','pavani','ankitha','ujju')
+'''
+'''def display(**products):
+    print(products)
+
+display(bag=5000)
+display(bag=5000,book=30)
+display(bag=5000,book=30,bottle=50)
+'''
+'''def display(n):
+    n=n+10
+    print('Inside:',n)'''
+
+'''l = [1,2,3,4]
+max=20
+sum = 10
+print(sum)'''
+
+
+'''def display(n):
+    n[5]=6
+    print('inside:',n)
+
+n={1:2,3:4}
+display(n)
+print('Outside:',n)'''
+
+
+   
+
+
+
+
+
+
+
+
+
+
